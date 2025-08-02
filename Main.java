522dcbf49a7705faaa903d61f4b1e258461fb8e4
Mou'ath <4281148@myuwc.ac.za>
2025-08-02 16:43:53 +0200

    16:44

--- Main.java
+++ Main.java
@@ -25,7 +25,6 @@ public class Main {
                     continue;
                 }
                 String[] parts = line.split(",", -1);
-                if (parts.length < 15) continue;
                 data.add(parts);
             }
             br.close();
